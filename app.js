b852e7332500acf25ad485e57d20371ae9bdf4ef
Mike <mcleary03@gmail.com>
2017-04-14 00:20:49 +0000

    set up mongodb, can now add campgrounds to db

--- app.js
+++ app.js
@@ -1,30 +1,64 @@
-var express = require("express")
-var app = express()
-var bodyParser = require("body-parser")
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose")
 
+// Creates the yelp_camp DB if not already there
+mongoose.connect("mongodb://localhost/yelp_camp")
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+})
+
+// Compile schema into a model that has methods(create, find, etc.)
+var Campground = mongoose.model("Campground", campgroundSchema)
+
+// Campground.create(
+//     {
+//         name: "Granite Hill", 
+//         image: "https://farm9.staticflickr.com/8314/7968774876_11eafbfbb7.jpg"
+//     }, (err, campground) => {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log("NEWLY CREATED CAMPGROUND: ")
+//             console.log(campground)
+//         }
+//     }
+// )
 
 app.get("/", (req, res) => {
     res.render("landing")
 })
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-    {name: "Granite Hill", image: "https://farm9.staticflickr.com/8314/7968774876_11eafbfbb7.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
-]
-
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds", { campgrounds })
+    // Get all campgrounds from DB
+    Campground.find({}, (err, campgrounds) => {
+        if (err) {
+            console.log(err)
+        } else {
+                res.render("campgrounds", { campgrounds })
+        }
+    })
 })
 
 app.post("/campgrounds", (req, res) => {
     var name = req.body.name
     var image = req.body.image
     var newCampground = { name, image }
-    campgrounds.push(newCampground)
-    res.redirect("/campgrounds")
+    // Create a new campground and save to DB
+    Campground.create(newCampground, (err, newlyCreated) => {
+        if (err) {
+            console.log(err)
+        } else {
+            // Redirect back to campgrounds page
+            res.redirect("/campgrounds")
+        }
+    })
 })
 
 app.get("/campgrounds/new", (req, res) => {
